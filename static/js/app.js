90a536dd6f9ded03b80f7f13d29cd770d2f19de9
nitish <nitish.singh158@gmail.com>
2020-11-07 21:28:04 -0700

    added func updateTable

--- static/js/app.js
+++ static/js/app.js
@@ -60,16 +60,18 @@ function updateFilters() {
   
     // 9. Loop through all of the filters and keep any data that
     // matches the filter values
-      filters.forEach((key,value) => {
-        filteredData = filteredData.filter(row => row.key===value);
-      })
+    
+      // Object.entries(filters).forEach( ([key,value]) => {
+      //   filteredData = filteredData.filter(row => row[key] === value);
+      //   });
+      
   
     // 10. Finally, rebuild the table using the filtered data
     buildTable(filteredData)
   }
   
   // 2. Attach an event to listen for changes to each filter
-  d3.selectAll("input").on("change",updateFilters)
+  d3.selectAll("input").on("change",updateFilters);
   
   // Build the table when the page loads
   buildTable(tableData);
